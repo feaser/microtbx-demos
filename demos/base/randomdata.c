a52665ff4fd22440cd686748fa26804db9c7a5da
Frank Voorburg <voorburg@feaser.com>
2022-06-11 20:59:39 +0200

    Completed the random data demo application.

--- demos/base/randomdata.c
+++ demos/base/randomdata.c
@@ -1,6 +1,12 @@
 /************************************************************************************//**
 * \file         demos/base/randomdata.c
-* \brief        Demo application source file.
+* \brief        Demonstrates how to use the random number generator to fill a data buffer
+*               with random data.
+* \details      The application fills two different data buffers with random data:
+*                 1. A byte array declared with a fixed size at compile time.
+*                 2. A byte array dynamically allocated on the heap.
+*               The benefit of the 2nd approach is that you can size the data array at
+*               run-time.
 * \internal
 *----------------------------------------------------------------------------------------
 *                          C O P Y R I G H T
@@ -40,9 +46,31 @@
 
 
 /****************************************************************************************
+* Macro definitions
+****************************************************************************************/
+/** \brief Size of the buffer for storing randomly generated data. */
+#define DEMO_BUFFER_SIZE    (32U)
+
+
+/****************************************************************************************
 * Function prototypes
 ****************************************************************************************/
 static uint32_t DemoSeedInitHandler(void);
+static void     DemoDisplayData(uint8_t const * data, size_t len, size_t bytesPerLine);
+
+
+/****************************************************************************************
+* Local data declarations
+****************************************************************************************/
+/** \brief Buffer for storing randomly generated data. Its size is fixed at compile
+ *         time.
+ */
+static uint8_t compileTimeBuffer[DEMO_BUFFER_SIZE];
+
+/** \brief Buffer for storing randomly generated data. Memory for the buffer is
+ *         dynamically allocated from the heap at run-time.
+ */
+static uint8_t * runTimeBuffer;
 
 
 /************************************************************************************//**
@@ -52,19 +80,44 @@ static uint32_t DemoSeedInitHandler(void);
 ****************************************************************************************/
 void DemoMain(void)
 {
-  uint32_t numbers[8];
-  size_t   idx;
+  uint32_t * dataPtr;
+  size_t     idx;
 
   /* Register the application specific seed initialization handler.*/
   TbxRandomSetSeedInitHandler(DemoSeedInitHandler);
 
-  /* Generate some random numbers and send them to the standard output. */
-  for (idx = 0; idx < (sizeof(numbers)/sizeof(numbers[0])); idx++)
+  /* Fill the compile-time allocated buffer with random data, 32 bits at a time. */
+  dataPtr = (uint32_t *)compileTimeBuffer;
+  for (idx = 0; idx < DEMO_BUFFER_SIZE/sizeof(uint32_t); idx++)
   {
-    /* Get a new random number. */
-    numbers[idx] = TbxRandomNumberGet();
-    /* Print the value. */
-    printf("Random number %u: %u.\n", idx+1, (unsigned int)numbers[idx]);
+    dataPtr[idx] = TbxRandomNumberGet();
+  }
+  /* Display the generated data on the standard output. */
+  printf("\nRandom data stored in the compile-time allocated buffer:\n");
+  DemoDisplayData(compileTimeBuffer, DEMO_BUFFER_SIZE, 16U);
+
+  /* Allocate memory for the run-time allocated buffer from the heap. The benefit of this
+   * approach is that you can configure its size at run-time. Note that you cannot free
+   * the memory using the heap module. However, such functionality is possible by using
+   * the memory pool module.
+   */
+  runTimeBuffer = TbxHeapAllocate(DEMO_BUFFER_SIZE);
+  /* Assert memory allocation. If it failed, increase the heap with configuration macro
+   * TBX_CONF_HEAP_SIZE in "tbx_conf.h".
+   */
+  TBX_ASSERT(runTimeBuffer != NULL);
+  /* Only continue if memory allocation was successful. */
+  if (runTimeBuffer != NULL)
+  {
+    /* Fill the run-time allocated buffer with random data, 32 bits at a time. */
+    dataPtr = (uint32_t *)runTimeBuffer;
+    for (idx = 0; idx < DEMO_BUFFER_SIZE/sizeof(uint32_t); idx++)
+    {
+      dataPtr[idx] = TbxRandomNumberGet();
+    }
+    /* Display the generated data on the standard output. */
+    printf("\nRandom data stored in the run-time allocated buffer:\n");
+    DemoDisplayData(runTimeBuffer, DEMO_BUFFER_SIZE, 16U);
   }
 
   /* Enter infinite program loop. */
@@ -90,6 +143,45 @@ static uint32_t DemoSeedInitHandler(void)
   /* Pass the request on to the board support package. */
   return BspGetSeed();
 } /*** end of DemoSeedInitHandler ***/
+
+
+/************************************************************************************//**
+** \brief     Displays the data as hexadecimal characters on the standard output.
+** \param     data Pointer to the data bytes to display.
+** \param     len Total number of bytes in the data buffer that should be displayed.
+** \param     bytesPerLine Number of bytes to display on one line.
+**
+****************************************************************************************/
+static void DemoDisplayData(uint8_t const * data, size_t len, size_t bytesPerLine)
+{
+  size_t idx;
+
+  /* Verify parameters. */
+  TBX_ASSERT((data != NULL) && (len > 0U) && (bytesPerLine > 0U));
+
+  /* Only continue with valid parameters. */
+  if ((data != NULL) && (len > 0U) && (bytesPerLine > 0U))
+  {
+    /* Loop through the bytes. */
+    for (idx = 0; idx < len; idx++)
+    {
+      /* Display the byte as a hexadecimal comprising of 2 characters. */
+      printf("%02X ", data[idx]);
+      /* Time to move on to the next line? */
+      if (((idx + 1) % bytesPerLine) == 0U)
+      {
+        /* Move on to the next line. */
+        printf("\n");
+      }
+    }
+    /* Final new line character needed? */
+    if ((idx % bytesPerLine) != 0U)
+    {
+      /* Move on to the next line. */
+      printf("\n");
+    }
+  }
+} /*** end of DemoDisplayData ***/
 
 
 /*********************************** end of randomdata.c *******************************/
