7a8e8f9b65eb9bc127380494e437bdd3694f324c
Frank Voorburg <voorburg@feaser.com>
2022-06-14 10:41:56 +0200

    Added framework for the sort list demo.

---
+++ demos/base/sortlist.c
@@ -0,0 +1,59 @@
+/************************************************************************************//**
+* \file         demos/base/sortlist.c
+* \brief        Demonstrates how to sort items in a linked list.
+* \details      TODO
+* \internal
+*----------------------------------------------------------------------------------------
+*                          C O P Y R I G H T
+*----------------------------------------------------------------------------------------
+*   Copyright (c) 2022 by Feaser     www.feaser.com     All rights reserved
+*
+*----------------------------------------------------------------------------------------
+*                            L I C E N S E
+*----------------------------------------------------------------------------------------
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+* 
+* The above copyright notice and this permission notice shall be included in all
+* copies or substantial portions of the Software.
+* 
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+*
+* \endinternal
+****************************************************************************************/
+
+/****************************************************************************************
+* Include files
+****************************************************************************************/
+#include <stdio.h>                               /* C standard input/output            */
+#include "microtbx.h"                            /* MicroTBX library                   */
+#include "bsp.h"                                 /* Board support package header.      */
+
+
+/************************************************************************************//**
+** \brief     Entry point for the demo application. Called by main() after the 
+**            initialization completed. Contains the infinite program loop.
+**
+****************************************************************************************/
+void DemoMain(void)
+{
+  /* Enter infinite program loop. */
+  while (1)
+  {
+    /* Nothing more to do for this particular demo. */
+    ;  
+  }
+} /*** end of DemoMain ***/
+
+
+/*********************************** end of sortlist.c *********************************/
